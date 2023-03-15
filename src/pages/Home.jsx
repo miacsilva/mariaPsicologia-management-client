d3512cd7ea8b4784a1a5ac6b012ef61c6d7c7e50
Tiago Rato <tiagorato13@hotmail.com>
2023-03-15 18:11:32 +0000

    minor changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -13,11 +13,8 @@ function Home() {
     <>
       <AboutHome />
       <MonthlySubject />
-      <NavLink to="/monthlySubjects">
-            <button className="monthlySeeMoreBtn">See past months «</button>
-          </NavLink>
     </>
   );
 }
 
-export default Home; 
+export default Home;
