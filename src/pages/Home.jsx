335848cf365bbd6c60ed3f69efdbd174f505326f
Tiago Rato <tiagorato13@hotmail.com>
2023-03-16 10:13:10 +0000

    redirect

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -64,90 +64,86 @@ function Home() {
             Psicologia | Desenvolvimento Pessoal | muito mais....
           </h2>
         </div>
-
-        <div className="iconContainer">
-          <img src={scroll} className="scroll" alt="scroll icon" />
-        </div>
+      </div>
+      <div className="iconContainer">
+        <img src={scroll} className="scroll" alt="scroll icon" />
       </div>
 
       {books.length && (
-
         <div className="carousselContainer">
-        <div
-          id="carouselExampleInterval"
-          className="carousel slide carrossel"
-          data-bs-ride="carousel"
-        >
-          <div className="carousel-inner carrossel2">
-            <NavLink to="/about">
-              <div className="carousel-item active" data-bs-interval="4000">
-                <img
-                  src={carrossel1}
-                  className="d-block w-100 carouselImage"
-                  alt="book1"
-                />
-              </div>
-            </NavLink>
-            <NavLink to="/books">
-              <div className="carousel-item" data-bs-interval="4000">
-                <img
-                  src={carrossel2}
-                  className="d-block w-100 carouselImage"
-                  alt="book2"
-                />
-              </div>
-            </NavLink>
+          <div
+            id="carouselExampleInterval"
+            className="carousel slide carrossel"
+            data-bs-ride="carousel"
+          >
+            <div className="carousel-inner carrossel2">
+              <NavLink to="/about">
+                <div className="carousel-item active" data-bs-interval="4000">
+                  <img
+                    src={carrossel1}
+                    className="d-block w-100 carouselImage"
+                    alt="book1"
+                  />
+                </div>
+              </NavLink>
+              <NavLink to="/books">
+                <div className="carousel-item" data-bs-interval="4000">
+                  <img
+                    src={carrossel2}
+                    className="d-block w-100 carouselImage"
+                    alt="book2"
+                  />
+                </div>
+              </NavLink>
 
-            <NavLink to="/appointments">
-              <div className="carousel-item" data-bs-interval="4000">
-                <img
-                  src={carrossel3}
-                  className="d-block w-100 carouselImage"
-                  alt="fnac2"
-                />
-              </div>
-            </NavLink>
-            <NavLink to="/contacts">
-              <div className="carousel-item" data-bs-interval="4000">
-                <img
-                  src={carrossel4}
-                  className="d-block w-100 carouselImage"
-                  alt="fnac2"
-                />
-              </div>
-            </NavLink>
+              <NavLink to="/appointments">
+                <div className="carousel-item" data-bs-interval="4000">
+                  <img
+                    src={carrossel3}
+                    className="d-block w-100 carouselImage"
+                    alt="fnac2"
+                  />
+                </div>
+              </NavLink>
+              <NavLink to="/contacts">
+                <div className="carousel-item" data-bs-interval="4000">
+                  <img
+                    src={carrossel4}
+                    className="d-block w-100 carouselImage"
+                    alt="fnac2"
+                  />
+                </div>
+              </NavLink>
+            </div>
+            <button
+              className="carousel-control-prev"
+              type="button"
+              data-bs-target="#carouselExampleInterval"
+              data-bs-slide="prev"
+            >
+              <span
+                className="carousel-control-prev-icon"
+                aria-hidden="true"
+              ></span>
+              <span className="visually-hidden">Previous</span>
+            </button>
+            <button
+              className="carousel-control-next"
+              type="button"
+              data-bs-target="#carouselExampleInterval"
+              data-bs-slide="next"
+            >
+              <span
+                className="carousel-control-next-icon"
+                aria-hidden="true"
+              ></span>
+              <span className="visually-hidden">Next</span>
+            </button>
           </div>
-          <button
-            className="carousel-control-prev"
-            type="button"
-            data-bs-target="#carouselExampleInterval"
-            data-bs-slide="prev"
-          >
-            <span
-              className="carousel-control-prev-icon"
-              aria-hidden="true"
-            ></span>
-            <span className="visually-hidden">Previous</span>
-          </button>
-          <button
-            className="carousel-control-next"
-            type="button"
-            data-bs-target="#carouselExampleInterval"
-            data-bs-slide="next"
-          >
-            <span
-              className="carousel-control-next-icon"
-              aria-hidden="true"
-            ></span>
-            <span className="visually-hidden">Next</span>
-          </button>
-        </div>
-
         </div>
       )}
       <AboutHome />
       <MonthlySubject />
-      
     </>
   );
 }
