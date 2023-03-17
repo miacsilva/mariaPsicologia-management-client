b34294643a7baac61603ad7befa061756efe2547
miacsilva <miacsilvac@hotmail.com>
2023-03-17 00:37:26 +0000

    mapsEmbedded

--- src/pages/Contacts.jsx
+++ src/pages/Contacts.jsx
@@ -59,6 +59,7 @@ function Contacts() {
                 <p>{contacts[0].address}</p>
               </div>
             </section>
+            
             <div className="separator"></div>
             <section className="contactsSectionLinks">
               <NavLink target="_blank" to={contacts[0].facebook}>
@@ -68,6 +69,9 @@ function Contacts() {
                 <img src={instagram} alt="" className="iconContacts" />
               </NavLink>
             </section>
+            <div className="maps">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2985.6976368430887!2d-8.425973084717468!3d41.55414357924915!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd24feebd5dae855%3A0x47df5853823876fe!2sAv.%20Gen.%20Norton%20de%20Matos%2013%203%C2%BA%20Andar%20SALA%2014%2C%204700-978%20Braga!5e0!3m2!1spt-PT!2spt!4v1679012813017!5m2!1spt-PT!2spt" width="450" height="400" style={{border:0,  borderRadius: "20px"}} allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            </div>
           </section>
         </>
       )}
